da4978bbf367af5d0a4cb0c50e40180f63021b4a
Peter Leaver <166836879+ptreedev@users.noreply.github.com>
2026-03-22 16:23:12 +0000

    add logic to display associated coins

--- static/js/duties.js
+++ static/js/duties.js
@@ -9,9 +9,21 @@ async function openDutyModal(dutyId) {
 
     try {
         const response = await fetch(`${API_URL}/duty/${dutyId}`);
-        const data = await response.json();
-        nameEl.textContent = data.name;
-        descriptionEl.textContent = data.description;
+        const duty = await response.json();
+        const coins = duty.coins;
+        nameEl.textContent = duty.name;
+        descriptionEl.textContent = duty.description;
+            if (coins) {
+                const coinsEl = document.createElement('h4');
+                coinsEl.textContent = 'Associated Coins: ';
+                for (const coin of coins) {
+                    const coinEl = document.createElement('p');
+                    coinEl.textContent = coin.name;
+                    coinsEl.appendChild(coinEl);
+                }
+                descriptionEl.appendChild(coinsEl);
+            }
+
     } catch (error) {
         nameEl.textContent = 'Error loading duty.';
         console.error('Error fetching duty data:', error);
